efc036e936a298237fe592fdc886bdb02e9c1980
Greg Fields <fieldsdev06@gmail.com>
2025-05-18 05:46:12 +0300

    feat: Stats logic ready

--- BusinessLogic/Extensions.cs
+++ BusinessLogic/Extensions.cs
@@ -9,6 +9,7 @@ public static class Extensions
     public static IServiceCollection AddBusinessLogic(this IServiceCollection serviceCollection)
     {
         serviceCollection.AddScoped<IDayTaskService, DayTaskService>();
+        serviceCollection.AddScoped<IStatisticsService, StatisticsService>();
         serviceCollection.AddScoped<IUserService, UserService>();
         serviceCollection.AddScoped<IJwtService, JwtService>();
         serviceCollection.AddScoped<ICurrentUserService, CurrentUserService>();
---
+++ BusinessLogic/Services/StatisticsService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using BusinessLogic.Interfaces;
+using DataAccess.Interfaces;
+
+namespace BusinessLogic.Services;
+internal class StatisticsService: IStatisticsService
+{
+    private readonly ICategoryRepository _repository;
+    private readonly ICurrentUserService _currentUserService;
+
+    public StatisticsService(ICategoryRepository repository, ICurrentUserService currentUserService)
+    {
+        _repository = repository;
+        _currentUserService = currentUserService;
+    }
+    public async Task<IEnumerable<StatisticsDto>> GetStatisticsAsync(CancellationToken cancellationToken = default)
+    {
+        var user = await _currentUserService.GetCurrentUserAsync();
+        var categories = await _repository.GetByUserIdAsync(user.Id, cancellationToken);
+
+        return categories.Select(category => {
+            return new StatisticsDto
+            {
+                Id = category.Id,
+                CountSuccess = category.CountSuccess,
+                CountMiddle = category.CountMiddle,
+                CountFailure = category.CountFailed,
+                Title = category.Title
+            };
+        });
+    }
+}
+
